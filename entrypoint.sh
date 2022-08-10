816d90fb37d6662a63031ccb844ffa5c76f8aced
Ryota Yoshikawa <yoshikawa@topotal.com>
2022-08-10 16:07:41 +0900

    Add /github/workspace to safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
+SAFE_GITHUB_ACTION_DIRECTORY="/github/workspace"
 CODECOMMIT_URL="${INPUT_CODECOMMIT_URL}"
+
+git config --global --add safe.directory ${SAFE_GITHUB_ACTION_DIRECTORY}
 
 git config --global credential."${CODECOMMIT_URL}".helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
